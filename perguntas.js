a52fcc0eec1cd4bdbd07952bec66c749d650f252
okaulokaul <victorbaldinivb@gmail.com>
2024-11-25 10:04:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cartas',
+    'Qual o nome do naipe em formato de coração?',
+    'O nome do naipe é Copas!'
+)
+
+criaCartao(
+    'Programação',
+    'Em qual linguagem esse site foi feito?',
+    'Esse site foi feito em JavaScript!'
+)
+
+criaCartao(
+    'Profissões',
+    'Qual a profissao que da aulas em escolas?',
+    'A profissão é Professor!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz TUDO em Inglês?',
+    'TUDO em ingles é everything!'
+)
